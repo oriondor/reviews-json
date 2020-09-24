19c4090af910307042ce03fa2f5eab311ad6a033
fullname <you@example.com>
2020-09-24 15:31:59 +0300

    different configs and db

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-шьзщке щы
+import os
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
